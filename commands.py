91f0462d268d0cd78cd10c62b0e96798aeaaf2e6
LeonKant <lkant1@mail.fresnostate.edu>
2022-10-27 21:25:34 -0700

    added commments

--- commands.py
+++ commands.py
@@ -2,14 +2,18 @@ print ("Enter message or command")
 
 i = True
 while i:
-    lst = input("> ").split(' ', 1)
-    if lst[0] == '-pm':
+    lst = input("> ").split(' ', 1) #prompt 
+
+    if lst[0] == '-pm':		    #private message
         print("private message: " + lst[1] + "\n")
-    elif lst[0] == '-q':
+
+    elif lst[0] == '-q':	    #quit program
         i = False
         print("Exiting program")
-    elif lst[0] == '-h':
+
+    elif lst[0] == '-h':	    #shows commands
         print("-pm: send private message")
         print("-q: quit program" + "\n")
-    else:
+
+    else:			    #just print message if no command
         print(' '.join(lst) + "\n")
